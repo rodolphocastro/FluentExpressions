15662f3bb448ffbce66cfed57371132c5cb426da
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-04-29 17:59:18 -0300

    Adicionar BaseToken

---
+++ src/FluentExpressions.Core/Tokens/BaseToken.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FluentExpressions.Core.Tokens
+{
+    /// <summary>
+    /// Token para construção de Regex.
+    /// </summary>
+    public abstract class BaseToken : IEquatable<BaseToken>
+    {
+        protected readonly string _value;
+
+        protected BaseToken(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Um valor deve ser definido", nameof(value));
+            }
+
+            _value = value;
+        }
+
+        public string Value => _value;
+
+        public bool Equals(BaseToken other) => other.Value.Equals(_value);
+        public override string ToString() => _value;
+
+        public static implicit operator string(BaseToken token) => token.Value;
+    }
+}
---
+++ tests/Tests.Core/Tokens/BaseTokenTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+using AutoFixture.Xunit2;
+
+using FluentAssertions;
+
+using FluentExpressions.Core.Tokens;
+
+using Xunit;
+
+namespace Tests.Core.Tokens
+{
+    public class MyToken : BaseToken
+    {
+        public MyToken(string value) : base(value)
+        {
+        }
+    }
+
+    public class BaseTokenTests
+    {
+        [Fact]
+        public void Construct_EmptyValue_ArgumentException()
+        {
+            // Arrange
+            // Act
+            Action act = () => _ = new MyToken(string.Empty);
+
+            // Assert
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Theory, AutoData]
+        public void GetValue([Frozen] string expected, MyToken subject)
+        {
+            // Arrange
+            // Act
+            string result = subject.Value;
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory, AutoData]
+        public void ToString_Returns_Value(MyToken subject)
+        {
+            // Arrange
+            // Act
+            string result = subject.ToString();
+
+            // Assert
+            result.Should().BeEquivalentTo(subject.Value);
+        }
+
+        [Theory, AutoData]
+        public void EqualsToken(MyToken subject)
+        {
+            // Arrange
+            var copy = new MyToken(subject.Value);
+            // Act
+            bool result = copy.Equals(subject);
+
+            // Assert
+            copy.Should().BeEquivalentTo(subject);
+            result.Should().BeTrue();
+        }
+
+        [Theory, AutoData]
+        public void Theory(MyToken subject)
+        {
+            // Arrange
+            // Act
+            string result = subject;
+
+            // Assert
+            result.Should().BeEquivalentTo(subject.Value);
+        }
+    }
+}
