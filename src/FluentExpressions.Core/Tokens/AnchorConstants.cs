e73883bffef6e94f525e151809a24c989aa101dd
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-05-02 13:28:29 -0300

    Adicionar constantes com anchors de Regex

---
+++ src/FluentExpressions.Core/Tokens/AnchorConstants.cs
@@ -0,0 +1,48 @@
+﻿namespace FluentExpressions.Core.Tokens
+{
+    /// <summary>
+    /// Constantes de Anchors.
+    /// </summary>
+    public static class AnchorConstants
+    {
+        /// <summary>
+        /// Start of a Match (\G).
+        /// </summary>
+        public const string _startOfMatch = @"\G";
+
+        /// <summary>
+        /// Start of a String (^).
+        /// </summary>
+        public const string _startOfString = @"^";
+
+        /// <summary>
+        /// Alternative Start of a String (\A).
+        /// </summary>
+        public const string _altStartOfString = @"\A";
+
+        /// <summary>
+        /// End of a String ($).
+        /// </summary>
+        public const string _endOfString = @"$";
+
+        /// <summary>
+        /// Alternative End of a String (\Z).
+        /// </summary>
+        public const string _altEndOfString = @"\Z";
+
+        /// <summary>
+        /// Absolute End of a String (\z).
+        /// </summary>
+        public const string _absoluteEndOfString = @"\z";
+
+        /// <summary>
+        /// Word boundary (\b).
+        /// </summary>
+        public const string _wordBoundary = @"\b";
+
+        /// <summary>
+        /// Non-Word boundary (\B).
+        /// </summary>
+        public const string _nonWordBoundary = @"\B";
+    }
+}
